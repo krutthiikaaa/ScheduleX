89948e66d311ad8bdcb96bc520e8ef59e40af628
N.Nitya <nityanara4@gmail.com>
2026-06-30 14:11:50 +0530

    Configure CORS for deployed frontend

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,15 @@ const Habit = require('./models/Habit');
 const Goal = require('./models/Goal');
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://schedule-x-krutthiikaaas-projects.vercel.app/"
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
